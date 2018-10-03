5640ac1c49282b891c76fe93a14f9e1b46a84c9f
Amira Chaib <43606565+Rollo5011@users.noreply.github.com>
2018-10-03 14:45:51 +0200

    Variables
    Just to start this somehow

---
+++ QuizStart.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+
+<html>
+    
+    <head>
+        <meta charset="UTF-8">
+        <title>Meme Quiz</title>
+    </head>
+    
+    <body>
+        <?php
+            $_SESSION[ScumbagSteve] = 0;
+            $_SESSION[BongoCat] = 0;
+            $_SESSION[SlapTheCar] = 0;
+            $_SESSION[MindBlown] = 0;
+            
+            
+        ?>
+    </body>
+</html>
