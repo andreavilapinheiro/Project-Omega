b3fb626b4b7a66dc07b9bcfcb36d46a70893b444
Amira Chaib <43606565+Rollo5011@users.noreply.github.com>
2018-10-03 15:29:35 +0200

    QuizStart Working!
    Yeeeeei

--- QuizStart.php
+++ QuizStart.php
@@ -15,28 +15,33 @@
         
         <?php
         
-            $_SESSION[ScumbagSteve] = 0;
-            $_SESSION[BongoCat] = 0;
-            $_SESSION[SlapTheCar] = 0;
-            $_SESSION[MindBlown] = 0;
+            $_SESSION["ScumbagSteve"] = 0;
+            $_SESSION["BongoCat"] = 0;
+            $_SESSION["SlapTheCar"] = 0;
+            $_SESSION["MindBlown"] = 0;
             
             $value = $_POST["Color"];
             
             switch ($value){
                 case "red": 
-                    $_SESSION[ScumbagSteve]++;
+                    $_SESSION["ScumbagSteve"]++;
                 break;
                 case "green":
-                    $_SESSION[BongoCat]++;
+                    $_SESSION["BongoCat"]++;
                 break;
                 case "yellow":
-                    $_SESSION[SlapTheCar]++;
+                    $_SESSION["SlapTheCar"]++;
                 break;
                 case "blue":
-                    $_SESSION[MindBlown]++;
+                    $_SESSION["MindBlown"]++;
                 break;
                 default : echo"Error";
             }
+            
+            echo"$_SESSION[ScumbagSteve]<br>"
+              . "$_SESSION[BongoCat]<br>"
+              . "$_SESSION[SlapTheCar]<br>"
+              . "$_SESSION[MindBlown]<br>";
         ?>
     </body>
 </html>
